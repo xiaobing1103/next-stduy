f612116af5fddbc234a0d1eef9234f88c73a5206
xiaobing1103 <472427503@qq.com>
2024-04-25 18:08:16 +0800

    提交代码 prisma

---
+++ src/app/auth.ts
@@ -0,0 +1,28 @@
+import NextAuth from "next-auth";
+import Github from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { db } from "@/db";
+
+const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
+
+const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
+
+if (!GITHUB_CLIENT_ID || !GITHUB_CLIENT_SECRET) {
+  throw Error("没有github验证id！！！");
+}
+
+NextAuth({
+  adapter: PrismaAdapter(db),
+  providers: [
+    Github({ clientId: GITHUB_CLIENT_ID, clientSecret: GITHUB_CLIENT_SECRET }),
+  ],
+  callbacks: {
+    // usually not needed,here we are fixing a bug in nextauth
+    async session({ session, user }: any) {
+      if (session && user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  },
+});
